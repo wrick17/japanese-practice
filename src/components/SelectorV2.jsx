eb65dd3ab11bd23381e2ed8ab162fdadfc1f2152
Pratyush Poddar <pratyush.poddar@sequoia.com>
2023-04-26 12:06:33 +0530

    batch selection

--- src/components/SelectorV2.jsx
+++ src/components/SelectorV2.jsx
@@ -17,25 +17,52 @@ export const SelectorV2 = ({ list, setList }) => {
     setList(newList);
   };
 
+  const toggleAll = (group, checked) => {
+    const newList = list.map((listItem) => {
+      if (listItem.title === group.title) {
+        return {
+          ...listItem,
+          rows: listItem.rows.map((row) => ({
+            ...row,
+            checked: !checked,
+          })),
+        };
+      }
+      return listItem;
+    });
+    setList(newList);
+  }
+
   return (
     <div className="selector-group">
-      {list.map((group) => (
-        <div className="selector-group-item" key={group.title}>
-          <ul>
-            {group.rows.map((item) => (
-              <li key={item.value}>
-                <button
-                  className={classNames("button", { selected: item.checked })}
-                  onClick={() => onSelect(item)}
-                >
-                  {item.value}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      {list.map((group) => {
+        const allChecked = group.rows.every((row) => row.checked);
+        return (
+          <div className="selector-group-item" key={group.title}>
+            <button
+              className={classNames("button batch", { selected: allChecked })}
+              onClick={() => toggleAll(group, allChecked)}
+            >
+              All
+            </button>
+            <ul>
+              {group.rows.map((item) => (
+                <li key={item.value}>
+                  <button
+                    className={classNames("button", { selected: item.checked })}
+                    onClick={() => onSelect(item)}
+                  >
+                    {item.value}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
+
+
 
